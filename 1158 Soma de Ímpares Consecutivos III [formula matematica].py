4c1b9012498298b068e81e8cefda420d2fd06c6c
Lucas Maieski <lucas.mski@gmail.com>
2026-05-02 13:38:35 -0300

    Create 1158 Soma de Ímpares Consecutivos III [formula matematica].py

---
+++ 1158 Soma de Ímpares Consecutivos III [formula matematica].py
@@ -0,0 +1,22 @@
+'''
+Leia um valor inteiro N que é a quantidade de casos de teste que vem a seguir. Cada caso de teste consiste de dois inteiros X e Y. Você deve apresentar a soma de Y ímpares consecutivos a partir de X inclusive o próprio X se ele for ímpar. Por exemplo:
+para a entrada 4 5, a saída deve ser 45, que é equivalente à: 5 + 7 + 9 + 11 + 13
+para a entrada 7 4, a saída deve ser 40, que é equivalente à: 7 + 9 + 11 + 13
+
+Entrada
+A primeira linha de entrada é um inteiro N que é a quantidade de casos de teste que vem a seguir. Cada caso de teste consiste em uma linha contendo dois inteiros X e Y.
+
+Saída
+Imprima a soma dos consecutivos números ímpares a partir do valor X.
+'''
+
+n = int(input())
+
+for _ in range(n):
+  x, y = map(int, input().split())
+  
+  inicio = x if x % 2 != 0 else x + 1
+
+  soma = (inicio + (inicio + ((y-1) * 2))) * y // 2 
+
+  print(soma)
