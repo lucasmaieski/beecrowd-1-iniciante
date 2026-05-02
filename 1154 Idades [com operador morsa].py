dd9a51ec0be8a54b0ad948db8cb8c7f68f5eb638
Lucas Maieski <lucas.mski@gmail.com>
2026-05-02 13:32:55 -0300

    Create 1154 Idades [com operador morsa].py

---
+++ 1154 Idades [com operador morsa].py
@@ -0,0 +1,19 @@
+'''Faça um algoritmo para ler um número indeterminado de dados, contendo cada um, a idade de um indivíduo. O último dado, que não entrará nos cálculos, contém o valor de idade negativa. Calcular e imprimir a idade média deste grupo de indivíduos.
+
+Entrada
+A entrada contém um número indeterminado de inteiros. A entrada será encerrada quando um valor negativo for lido.
+
+Saída
+A saída contém um valor correspondente à média de idade dos indivíduos.
+
+A média deve ser impressa com dois dígitos após o ponto decimal.'''
+
+idades = []
+
+while (n := int(input())) >= 0:
+  idades.append(n)
+
+if len(idades) > 0:
+  media = sum(idades) / len(idades)
+
+print(f"{media:.2f}")
